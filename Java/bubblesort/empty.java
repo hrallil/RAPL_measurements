61ebd4109459847a915f00b06b48887a9aa83dee
hrallil <mathias.larsen0@gmail.com>
2023-03-12 21:01:36 +0100

    sorted repo, added RAPL tool

---
+++ Java/bubblesort/empty.java
@@ -0,0 +1,12 @@
+import java.lang.Thread;
+
+public class empty{
+	public static void main(String[] args){
+		try{
+			Thread.sleep(1000);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+	}
+}
