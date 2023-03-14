6160edba74c43c5a62fc88c373ae1e0f336817eb
hrallil <mathias.larsen0@gmail.com>
2023-03-14 15:56:40 +0100

    Update Makefiles
    Make data
    (datasize: 25k,50k,75k,100k,200k,300k,400k,500k,600k,700k,800k,900k,1m)

--- Java/bubblesort/bubblesort.java
+++ Java/bubblesort/bubblesort.java
@@ -1,30 +1,38 @@
-import java.util.Random;
+import java.io.File;
+import java.util.Scanner;
 public class bubblesort{
 	//main mehtod
 	public static void main(String[] args ){
-		double[] arr =  makeArrDouble(1024);
-		System.out.print("Unsorted List: ");
-		
-		for(int i = 0; i<arr.length; i++){
-			System.out.print(arr[i] + ", ");
-		}
-		System.out.println();
-
+		int[] arr =  CSV_toArr(args[0],args[1]);
 		bubblesort(arr);
-		
-		System.out.print("sorted List: ");
-		for(int i = 0; i<arr.length; i++){
-			System.out.print(arr[i] + ", ");
-		}
-		System.out.println();
 	}
 
-	//INT sorting algorithm - overloaded
+	//method to read CSV files and convert them to Array
+	public static int[] CSV_toArr(String path, String fileSize){
+		int[] arr = new int[Integer.parseInt(fileSize)];
+		try{
+			Scanner sc = new Scanner(new File(path));
+			sc.useDelimiter(", |\n");
+			int i = 0;
+			while(sc.hasNext()){
+				arr[i] = Integer.parseInt(sc.next());
+				i++;
+			}
+			sc.close();
+			return arr;
+		}
+		catch(Exception e){
+			System.out.println(e);
+			System.out.println("Failed to read csv: " + path +"\n");
+		}
+		return arr;
+	}
+
+	//INT sorting algorithm
 	public static int[] bubblesort(int[] arr){
 		for(int i = 0; i < arr.length -1; i++){
 			for(int j = 0; j < arr.length - i - 1; j++){
 				if(arr[j] > arr[j+1]){
-
 					int temp = arr[j];
 					arr[j] = arr[j + 1];
 					arr[j + 1] = temp;
@@ -33,87 +41,4 @@ public class bubblesort{
 		}
 		return arr;
 	}
-	//LONG sorting algorithm, does nothing for now - overloaded
-	public static long[] bubblesort(long[] arr){
-		for(int i = 0; i < arr.length -1; i++){
-			for(int j = 0; j < arr.length - i - 1; j++){
-				if(arr[j] > arr[j+1]){
-
-					long temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-				}
-			}
-		}
-		return arr;
-	}
-	//FLOAT sorting algorithm, does nothing for now - overloaded
-	public static float[] bubblesort(float[] arr){
-		for(int i = 0; i < arr.length -1; i++){
-			for(int j = 0; j < arr.length - i - 1; j++){
-				if(arr[j] > arr[j+1]){
-
-					float temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-				}
-			}
-		}
-		return arr;
-	}
-	//DOUBLE sorting algorithm, does nothing for now - overloaded
-	public static double[] bubblesort(double[] arr){
-		for(int i = 0; i < arr.length -1; i++){
-			for(int j = 0; j < arr.length - i - 1; j++){
-				if(arr[j] > arr[j+1]){
-
-					double temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
-				}
-			}
-		}
-		return arr;
-	}
-	
-	//creates dataset_INT to sort
-	public static int[] makeArrInt(int size){
-		Random r = new Random();
-		int[] arr = new int[size];
-		for(int i=0; i < size; i++){
-			arr[i] = r.nextInt(Integer.MAX_VALUE);
-		}
-		return arr;
-	}
-	
-	//creates dataset_LONG to sort
-	public static long[] makeArrLong(int size){
-		Random r = new Random();
-		long[] arr = new long[size];
-		for(int i=0; i < size; i++){
-			arr[i] = r.nextLong();
-		}
-		return arr;
-	}
-	//creates dataset_FLOAT to sort
-	public static float[] makeArrFloat(int size){
-		Random r = new Random();
-		float[] arr = new float[size];
-		for(int i=0; i < size; i++){
-			arr[i] = r.nextFloat();
-		}
-		return arr;
-	}
-	//creates dataset_DOUBLE to sort
-	public static double[] makeArrDouble(int size){
-		Random r = new Random();
-		double[] arr = new double[size];
-		for(int i=0; i < size; i++){
-			arr[i] = r.nextDouble();
-		}
-		return arr;
-	}
-
-
-
 }
