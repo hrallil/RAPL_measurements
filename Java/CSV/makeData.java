dcf4b4cd68bfc7ab01ebd61603d8821ed0453c32
hrallil <mathias.larsen0@gmail.com>
2023-03-23 11:02:18 +0100

    update CSV

--- Java/CSV/makeData.java
+++ Java/CSV/makeData.java
@@ -3,22 +3,43 @@ import java.io.*;
 
 public class makeData{
 	public static void main(String[] args){
-		writeFile(args[0]);
+		writeFile(args[0],args[1]);
 	}
-	public static void writeFile(String s){
-		File f = new File("./data/" + Integer.parseInt(s) + ".csv");
+
+
+	// Write to file method. Will write acording to input from main. can write :sorted data, reverse sorted data, partially sorted data 
+	// and will default to random data
+	public static void writeFile(String s, String type){
+		int arraySize = Integer.parseInt(s);
+		File f = new File("./data/" + type + "-" + arraySize + ".csv");
 		Random r = new Random();
 		try{
 			PrintWriter w = new PrintWriter(f);
-
-			for(int i = 0; i<Integer.parseInt(s);i++){
-				w.println(r.nextInt());
+			
+			if(type=="sorted"){
+				for (int i = 0; i < arraySize; i++) {
+					w.println(i);
+				}
 			}
-			w.close();
+			if (type=="reverseSorted") {
+				for (int i = 0; i < arraySize; i++) {
+					w.println(arraySize - i);
+				}
+			}
+			if (type == "partialSorted") { //what does partially sorted mean ? 
+				for (int i = 0; i < arraySize; i++) {
+					w.println(arraySize - i);
+				}
+			}
+			if(type == "random" || type==null){
+				for(int i = 0; i<arraySize;i++){
+					w.println(r.nextInt());
+				}
+				w.close();
+			}
 		}
 		catch(Exception e){
 			e.printStackTrace();
 		}
-		
 	}
 }
--- Java/CountingSort/countingSort.java
+++ Java/CountingSort/countingSort.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Main {
-
     	public static void main(String[] args) {
         	int[] arr = CSV_toArr(args[0], args[1]); //{1,0,3,1,3,1};
 //        	System.out.println("before: "+ Arrays.toString(arr));
