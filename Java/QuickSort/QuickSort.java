e156f4482e0b126c9e85a9072f3302f40cedebdb
hrallil <mathias.larsen0@gmail.com>
2023-03-24 22:55:34 +0100

    Update QuickSort.java

--- Java/QuickSort/QuickSort.java
+++ Java/QuickSort/QuickSort.java
@@ -45,7 +45,7 @@ public class QuickSort{
 	}
 
 	public static int partition(int[] arr, int low, int high){
-		int pivot = middleOfThree(arr[0], arr[arr.length/2], arr[arr.length-1]);
+		int pivot = middleOfThree(arr[0], arr[(int)arr.length/2], arr[arr.length-1]);
 		int leftwall = low;
 		int temp;
 		for(int i = low+1; i <= high;i++){
