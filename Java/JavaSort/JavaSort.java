648eecb6fcdf226172aa093c1d07085345958c5a
SundmanDK <72975724+SundmanDK@users.noreply.github.com>
2023-04-14 13:02:58 +0200

    typo

--- Java/JavaSort/JavaSort.java
+++ Java/JavaSort/JavaSort.java
@@ -22,7 +22,7 @@ public class JavaSort{
 			FIleWriter fw = new FileWriter(f, true);
 			fw.write(usedMemAfter+",");
 			fw.close();
-		} close (Exception e){
+		} catch (Exception e){
 			System.out.println(e);
 		}
 		
