7f05fdc99a5dc2ab97c796ed4fb114fe05fe4e61
SundmanDK <72975724+SundmanDK@users.noreply.github.com>
2023-04-14 12:54:49 +0200

    memory log

--- Java/JavaSort/JavaSort.java
+++ Java/JavaSort/JavaSort.java
@@ -1,11 +1,31 @@
 import java.util.Scanner;
-import java.io.*;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Arrays;
 
 public class JavaSort{
 	public static void main(String[] args){
 		int[] arr = CSV_toArr(args[0],args[1]);
+		
+		Runtime rt = Runtime.getRuntime();
+		long totalMemBefore = rt.totalMemory();
+		long freeMemBefore  = rt.freeMemory();
+		long usedMemBefore  = totalMemBefore - freeMemBefore;
+		
 		Arrays.sort(arr);
+		
+		long totalMemAfter = rt.totalMemory();
+		long freeMemAfter  = rt.freeMemory();
+		long usedMemAfter  = (totalMemBefore - freeMemBefore) - usedMemBefore;
+		File f = new File("./results/BubbleSortMemory-PC#.csv"); //replace # with PC number
+		try{
+			FIleWriter fw = new FileWriter(f, true);
+			fw.write(usedMemAfter+",");
+			fw.close();
+		} close (Exception e){
+			System.out.println(e);
+		}
+		
 		/*
 		for(int i = 0; i<arr.length;i++){
 			System.out.println(arr[i]);
