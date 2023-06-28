fc797c85685d7ef32431c6f429a910f18cd8b4e9
SundmanDK <72975724+SundmanDK@users.noreply.github.com>
2023-06-28 12:54:00 +0200

    fix

--- Java/bubblesort/BubblesortBAD.java
+++ Java/bubblesort/BubblesortBAD.java
@@ -1,7 +1,7 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.util.Scanner;
-public class BubbleSort{
+public class BubbleSortBAD{
 	//main mehtod
 	public static void main(String[] args ){
 		int[] arr =  CSV_toArr(args[0],args[1]);
